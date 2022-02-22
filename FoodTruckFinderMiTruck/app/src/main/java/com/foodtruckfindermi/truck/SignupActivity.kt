35dedabdf39d697ee9ef6159fbad413df1337024
Alexander <covaalexander06@gmail.com>
2022-02-21 21:05:49 -0500

    Added Spinner, and rest of signup

--- FoodTruckFinderMiTruck/app/src/main/java/com/foodtruckfindermi/truck/SignupActivity.kt
+++ FoodTruckFinderMiTruck/app/src/main/java/com/foodtruckfindermi/truck/SignupActivity.kt
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 import com.github.kittinunf.fuel.Fuel
 
 class SignupActivity : AppCompatActivity() {
@@ -15,13 +14,37 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
 
         val signupButton = findViewById<Button>(R.id.signupButtonConfirm)
+        val nameEdit = findViewById<EditText>(R.id.truckNameEditText)
         val emailEdit = findViewById<EditText>(R.id.emailEdit)
         val passwordEdit = findViewById<EditText>(R.id.editPassword)
+
+        val food_types = resources.getStringArray(R.array.FoodTypes)
+        val food_spinner = findViewById<Spinner>(R.id.foodSpinner)
+
+        var selected_food = ""
+
+        if (food_spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, food_types)
+            food_spinner.adapter = adapter
+
+            food_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    selected_food = food_types[p2]
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    //TODO(RUN STUFF)
+                }
+            }
+        }
+
+
 
 
 
         signupButton.setOnClickListener {
-            Fuel.get("https://foodtruckfindermi.com/truck_auth?email=${emailEdit.text}&password=${passwordEdit.text}&type=LOGIN")
+            Fuel.get("https://foodtruckfindermi.com/truck_auth?name=${nameEdit.text}&email=${emailEdit.text}&password=${passwordEdit.text}&food=${selected_food}&type=LOGIN")
                 .response { _request, _response, result ->
                     val (bytes) = result
                     if (bytes != null) {
