41987ddb82ff15912b1d0311c609e95b46e81d6a
Alexander <covaalexander06@gmail.com>
2022-03-08 15:44:41 -0500

    added a basic ui start for the truck screen after login

--- FoodTruckFinderMiTruck/app/src/main/java/com/foodtruckfindermi/truck/LoginActivity.kt
+++ FoodTruckFinderMiTruck/app/src/main/java/com/foodtruckfindermi/truck/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.foodtruckfindermi.truck
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
@@ -18,17 +19,24 @@ class LoginActivity : AppCompatActivity() {
         val loginButton = findViewById<Button>(R.id.loginButtonConfirm)
 
         loginButton.setOnClickListener {
-            Fuel.get("http://foodtruckfindermi.com/truck-login?email=${emailEdit.text}&password=${passwordEdit.text}")
+            Fuel.get("http://foodtruckfindermi.com/truck-login?email=Truck1@gmail.com&password=Truck1")
                 .response { _request, _response, result ->
+
+                    Log.i("http", "sent request")
 
                     val (bytes) = result
                     if (bytes != null) {
                         var loginResult = "call ${String(bytes)}"
 
                         if (loginResult.equals("call true")) {
+                            Log.i("true", "true")
                             val intent = Intent(this, TruckActivity::class.java)
                             startActivity(intent)
+                        } else{
+                            Log.i("false", "false")
                         }
+                    } else {
+                        Log.i("http", "false")
                     }
                 }
         }
